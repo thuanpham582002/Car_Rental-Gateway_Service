17b65515b8ebc8dbbbd2c912003a48053588026f
thuanpt <tienthuan05082002@gmail.com>
2024-04-16 00:52:26 +0700

    setup complete for docker compose

--- src/main/kotlin/dev/noroom113/car_rent_management_gateway/GatewayServiceApplication.kt
+++ src/main/kotlin/dev/noroom113/car_rent_management_gateway/GatewayServiceApplication.kt
@@ -4,15 +4,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
-class CarRentManagementGatewayApplication{
-
-}
-// this is test commit message
+class CarRentManagementGatewayApplication
 
 fun main(args: Array<String>) {
     runApplication<CarRentManagementGatewayApplication>(*args)
+}
+
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health")
+    fun healthCheck(): String {
+        return "Gateway Service is up and running"
+    }
 }
